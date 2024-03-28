c5aade1da4d2d7289ab87b2e30f7b853e803a8a5
juchanhwang <ghkdwncks96@gmail.com>
2024-03-28 18:38:26 +0900

    refactor(hooks): export 값 명시하기

--- packages/design-system/src/hooks/index.tsx
+++ packages/design-system/src/hooks/index.tsx
@@ -1,2 +1,5 @@
-export * from './useContextData';
-export * from './useContextAction';
+export { useContextData } from './useContextData';
+export type { ContextData } from './useContextData';
+
+export { useContextAction } from './useContextAction';
+export type { ContextAction } from './useContextAction';
