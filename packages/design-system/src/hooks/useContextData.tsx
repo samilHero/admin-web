7a7f21dbea96854158fcc30619ea67a4de980ea0
juchanhwang <ghkdwncks96@gmail.com>
2024-03-30 16:29:11 +0900

    refactor(design-system/hooks): 훅의 파일 확장자을 .tsx로 수정

--- packages/design-system/src/hooks/useContextData.tsx
+++ packages/design-system/src/hooks/useContextData.tsx
@@ -3,19 +3,19 @@ import { useContext } from 'react';
 
 // 제네릭 타입 T를 사용하여, 어떤 타입의 컨텍스트든 처리할 수 있도록 합니다.
 // component 매개변수는 컴포넌트 이름을 나타내며, 오류 메시지에 사용됩니다.
-// actionContext 매개변수는 React의 Context 객체입니다.
-export const useContextAction = <T>(
+// dataContext 매개변수는 React의 Context 객체입니다.
+export const useContextData = <T,>(
   component: string,
-  actionContext: Context<T | null>,
+  dataContext: Context<T | null>,
 ) => {
-  const context = useContext(actionContext);
+  const context = useContext(dataContext);
 
   if (context === null) {
     const err = new Error(
-      `<${component} /> is missing a parent component that provides a ${actionContext.displayName || 'specified'} context.`,
+      `<${component} /> is missing a parent component that provides a ${dataContext.displayName || 'specified'} context.`,
     );
     if (Error.captureStackTrace) {
-      Error.captureStackTrace(err, useContextAction);
+      Error.captureStackTrace(err, useContextData);
     }
     throw err;
   }
@@ -23,4 +23,4 @@ export const useContextAction = <T>(
   return context;
 };
 
-export type ContextAction<T> = ReturnType<typeof useContextAction<T>>;
+export type ContextData<T> = ReturnType<typeof useContextData<T>>;
