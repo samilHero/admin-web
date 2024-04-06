9250b75f650d85205e7151cfc2488f2a387f741b
juchanhwang <ghkdwncks96@gmail.com>
2024-04-06 15:17:22 +0900

    chore(layout): 사용하지 않는 컴포넌트 삭제

--- packages/missionary-admin/src/app/page.tsx
+++ packages/missionary-admin/src/app/page.tsx
@@ -1,10 +1,3 @@
-import { CommonInput } from '@samilhero/design-system/src/components/index';
-
 export default function Home() {
-  return (
-    <main>
-      선교 상륙 작전1
-      <CommonInput />
-    </main>
-  );
+  return <main>선교 상륙 작전1</main>;
 }
