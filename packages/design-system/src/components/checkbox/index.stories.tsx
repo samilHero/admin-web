28eaed2d3945d0543f290c372e412b43edf242f2
juchanhwang <ghkdwncks96@gmail.com>
2024-04-15 21:11:28 +0900

    Feat: Checkbox 추가

---
+++ packages/design-system/src/components/checkbox/index.stories.tsx
@@ -0,0 +1,66 @@
+import { Checkbox } from './index';
+import type { Meta, StoryObj } from '@storybook/react';
+import { useState } from 'react';
+import styled from '@emotion/styled';
+
+const Icon = styled.span<{ checked: boolean }>`
+  position: relative;
+  display: inline-block;
+
+  width: 16px;
+  height: 16px;
+  margin-right: 0.5em;
+  border: 1px solid #000;
+
+  ${({ checked }) =>
+    checked &&
+    `
+    &::after {
+        content:'V';
+        font-size: 16px;
+        font-weight: bold;
+        width: 16px;
+        height: 16px;
+        text-align: center;
+        position: absolute;
+        left: 0;
+        top:0;
+    }
+  `}
+`;
+
+const meta: Meta<typeof Checkbox> = {
+  component: Checkbox,
+};
+
+export default meta;
+type Story = StoryObj<typeof Checkbox>;
+
+export const Primary: Story = {
+  render: (args) => {
+    const Story = () => {
+      const [checked, setChecked] = useState(false);
+
+      return (
+        <>
+          <Checkbox
+            checked={checked}
+            onChange={(newChecked) => {
+              console.log(newChecked);
+              setChecked(newChecked);
+            }}
+            value={'value'}
+          >
+            <Icon checked={checked} />
+            Checkbox
+          </Checkbox>
+        </>
+      );
+    };
+
+    return <Story />;
+  },
+  args: {
+    checked: false,
+  },
+};
