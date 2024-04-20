444213bfc570f5fe89700442c2ce830af8c0d9a8
장예찬 <ysnarmi2@gmail.com>
2024-04-20 23:06:47 +0900

    test

--- packages/missionary-admin/src/lib/EmotionProvider.tsx
+++ packages/missionary-admin/src/lib/EmotionProvider.tsx
@@ -2,14 +2,14 @@
 
 import { ThemeProvider } from '@emotion/react';
 
-// import { GlobalStyle } from '@/styles/Global';
+import { GlobalStyle } from '@/styles/Global';
 
 import { theme } from '../styles/theme';
 
 export function EmotionProvider({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider theme={theme}>
-      {/* <GlobalStyle /> */}
+      <GlobalStyle />
       {children}
     </ThemeProvider>
   );
