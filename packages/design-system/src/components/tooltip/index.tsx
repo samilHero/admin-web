9c21018828997e6a6e8f4d86d28ca79a139d12e6
장예찬 <ysnarmi2@gmail.com>
2024-04-20 22:42:28 +0900

    ci 에러수정

--- packages/design-system/src/components/tooltip/index.tsx
+++ packages/design-system/src/components/tooltip/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import type { ReactNode } from 'react';
 
-import { TooltipLayout, TooltipTrigger, TooltipText } from './ToolTipLayout';
+import { TooltipLayout, TooltipTrigger, TooltipText } from './TooltipLayout';
 
 interface TooltipProps {
   text: string;
