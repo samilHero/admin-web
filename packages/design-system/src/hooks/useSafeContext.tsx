6aa5c929df794dfc3a683908b390ec9ae27d9f3c
JuChan <ghkdwncks96@gmail.com>
2024-04-23 12:54:02 +0900

    Feat(design-system): useSafeContext hook 추가 (#15)
    - useContext()훅을 사용할 때 에러 핸들링을 하지 않는 문제를 해결하기 위한 작업

---
+++ packages/design-system/src/hooks/useSafeContext.tsx
@@ -0,0 +1,18 @@
+import type { Context } from 'react';
+import { useContext } from 'react';
+
+export const useSafeContext = <T,>(context: Context<T>): NonNullable<T> => {
+  const value = useContext(context);
+  if (value === undefined || value === null) {
+    const contextName = context.displayName || JSON.stringify(context, null, 2);
+    const err = new Error(
+      `${contextName}.Provider 태그 내부에서 useSafeContext 사용해주세요.`,
+    );
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(err, useSafeContext);
+    }
+    throw err;
+  }
+
+  return value;
+};
