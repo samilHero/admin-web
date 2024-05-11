9d44460a9ed14f9b4e66f7b7118ada904dd60bac
장예찬 <ysnarmi2@gmail.com>
2024-05-11 17:52:14 +0900

    SMH-51 Input : 디자인

--- packages/design-system/next.config.js
+++ packages/design-system/next.config.js
@@ -7,9 +7,6 @@ const nextConfig = {
   webpack: (config) => {
     config.module.rules.push({ // 웹팩설정에 로더 추가함
       test: /\.svg$/,
-      issuer: {
-        test: /\.(js|ts)x?$/,
-      },
       use: ['@svgr/webpack'],
     });
     return config
