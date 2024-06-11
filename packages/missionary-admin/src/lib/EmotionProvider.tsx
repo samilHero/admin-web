499047e45868ec5e12f564f36c9e4c16b55c97b7
juchanhwang <ghkdwncks96@gmail.com>
2024-06-11 16:45:18 +0900

    [SMH-89] chore(admin/next.config.js): @samilhero/design-system 워크스페이스 모듈 사용 및 next.confg에서 alias 선언 - design-system의 path alias를 찾지 못하는 문제가 있습니다. 앞으로 design-system에 경로가 추가될 때 alias를 선언해주면 됩니다.

--- packages/missionary-admin/src/lib/EmotionProvider.tsx
+++ packages/missionary-admin/src/lib/EmotionProvider.tsx
@@ -2,7 +2,7 @@
 
 import { ThemeProvider } from '@emotion/react';
 
-import { theme } from '@styles/theme';
+import { theme } from 'styles/theme';
 
 export function EmotionProvider({ children }: { children: React.ReactNode }) {
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
