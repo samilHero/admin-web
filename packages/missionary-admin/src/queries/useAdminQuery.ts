009f6008811c01fe446ddccfd8181e918a4a7374
장예찬 <ysnarmi2@gmail.com>
2024-06-16 16:50:27 +0900

    SMH-88 로그인 전 후 레이아웃

--- packages/missionary-admin/src/queries/useAdminQuery.ts
+++ packages/missionary-admin/src/queries/useAdminQuery.ts
@@ -2,17 +2,17 @@ import type {
   UseQueryOptions,
   UseMutationOptions,
 } from '@tanstack/react-query';
-import {
-  useMutation,
-  useQuery,
-} from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import useAdminAPI from 'apis'; // 파일 경로에 'apis'를 정확하게 맞춰주세요
 import { CHURCHES_LIST_QUERY, LOGIN_MUTATION } from './queryKeys';
 
 /**
  * 교회 정보 목록 조회
  */
-export const useGetChurchesQuery = (request: any, options?: UseQueryOptions) => {
+export const useGetChurchesQuery = (
+  request: any,
+  options?: UseQueryOptions,
+) => {
   const { getChurches } = useAdminAPI();
 
   const queryFn = async () => {
